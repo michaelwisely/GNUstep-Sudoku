ec1863f607ab1772209aa83987694c3cdac1b305
Michael Wisely <michaelwisely@gmail.com>
2009-12-25 21:47:09 -0600

    added the recursive function, still got to do the square availability checker

--- puzzleBoard.m
+++ puzzleBoard.m
@@ -23,5 +23,61 @@
   return self;
 }
 
+-(void) solve: (id) sender
+{
+  
+}
+
+-(Boolean) solvePuzzleBoardStartingAtRow:(NSInteger) row column:(NSInteger) column
+{
+  NSInteger i = 1;
+
+  while(![[self cellAtRow:row column:column] isEditable])
+  {
+    if(column < 8)
+    {
+      column+=1;
+    }
+    else
+    {
+      column = 0;
+      row+=1;
+    }
+  }
+
+  if(row > 8 || column > 8)
+  {
+    return true;
+  }
+
+  for(; i<10; i++)
+  {
+    if([self iCanPut:i atRow:row column:column])
+    {
+      [[self cellAtRow:row column:column] setStringValue:[NSString stringWithFormat:@"%c", i]];
+
+      if(column < 8)
+      {
+        if ([self solvePuzzleBoardStartingAtRow:row column: column+1])
+        {
+          return true;
+        }
+      }
+      else
+      {
+        if ([self solvePuzzleBoardStartingAtRow:row+1 column: 0])
+        {
+          return true;
+        }
+      }
+    }
+  }
+  if([[self cellAtRow:row column:column] isEditable])
+  {
+    [[self cellAtRow:row column:column] setStringValue:[NSString stringWithString:@"0"]]
+  }
+  return false;
+}
+
 
 @end
